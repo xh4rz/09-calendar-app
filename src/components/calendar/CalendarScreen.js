bb71f89958db71840ee4635836ec5802d9036056
Harold Gonzalez <xh4rz@hotmail.com>
2022-02-06 22:36:02 -0500

    6._React_Big_Calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
 import { Navbar } from '../ui/Navbar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+	{
+		title: 'Cumpleaños del jefe',
+		start: moment().toDate(),
+		end: moment().add(2, 'hours').toDate(),
+		bgcolor: '#fafafa'
+	}
+];
 
 export const CalendarScreen = () => {
 	return (
-		<div>
+		<div className="calendar-screen">
 			<Navbar />
+			<Calendar
+				localizer={localizer}
+				events={events}
+				startAccessor="start"
+				endAccessor="end"
+			/>
 		</div>
 	);
 };
