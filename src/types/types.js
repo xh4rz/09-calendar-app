fd51a2d641a4ac2745cd54337515fb54f9b2016f
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-14 19:29:48 -0500

    5._AuthReducer

--- src/types/types.js
+++ src/types/types.js
@@ -1,9 +1,18 @@
 export const types = {
 	uiOpenModal: '[ui] Open Modal',
 	uiCloseModal: '[ui] Close Modal',
+
 	eventSetActive: '[event] Set Active',
 	eventAddNew: '[event] Add New',
 	eventClearActiveEvent: '[event] Clear Active Event',
 	eventUpdated: '[event] Event Updated',
 	eventDeleted: '[event] Event Deleted',
+
+	authChecking: '[auth] Checking login state',
+	authCheckingFinish: '[auth] Finish checking login state',
+	authStartLogin: '[auth] Start login',
+	authLogin: '[auth] Login',
+	authStartRegister: '[auth] Start register',
+	authStartStartTokenRenew: '[auth] Start token renew',
+	authLogout: '[auth] Logout'
 };
