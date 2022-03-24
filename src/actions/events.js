38cec373664efbf39116ef738f128c2fd601e5a7
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-03-23 23:48:35 -0500

    7._Actualizar_el_evento

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { fetchConToken } from '../helpers/fetch';
 import { prepareEvents } from '../helpers/prepareEvents';
 import { types } from '../types/types';
@@ -41,7 +42,24 @@ export const eventClearActiveEvent = () => ({
 	type: types.eventClearActiveEvent
 });
 
-export const eventUpdated = (event) => ({
+export const eventStartUpdate = (event) => {
+	return async (dispatch) => {
+		try {
+			const resp = await fetchConToken(`events/${event.id}`, event, 'PUT');
+			const body = await resp.json();
+
+			if (body.ok) {
+				dispatch(eventUpdated(event));
+			} else {
+				Swal.fire('Error', body.msg, 'error');
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+};
+
+const eventUpdated = (event) => ({
 	type: types.eventUpdated,
 	payload: event
 });
